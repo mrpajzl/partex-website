51e058202698f30aecb8c08dfa7aea7d416a120b
mrpajzl <zraly.ondrej@icloud.com>
2026-07-15 09:08:08 +0200

    fix: block cms test subpaths in robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin", "/admin/", "/admin/*", "/cms-test", "/cms-test/", "/api/*"],
+        disallow: ["/admin", "/admin/", "/admin/*", "/cms-test", "/cms-test/", "/cms-test/*", "/api/*"],
       },
     ],
     sitemap: absoluteUrl("/sitemap.xml"),
